d5fcf7d823a95745993b5b067e3ffe192a9e8ef4
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-03-10 23:19:50 +0600

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -38,4 +38,59 @@
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                           
+    $mail->Host       = 'smtp.example.com';
+    $mail->SMTPAuth   = true;                                  
+    $mail->Username   = 'your_email@example.com';
+    $mail->Password   = 'your_email_password';
+    $mail->SMTPSecure = 'tls';         
+    $mail->Port       = 587;
+
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress('recipient@example.com', 'Recipient Name'); 
+
+    // Content
+    $mail->isHTML(true);                                 
+    $mail->Subject = $subject;
+    $mail->Body    = nl2br($message);
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
